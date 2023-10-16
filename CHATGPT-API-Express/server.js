1fc4d2d4212e395c5712ad3831f6a9ca5a155ee0
pulkitjhod <pulkit.kumar2020@vitbhopal.ac.in>
2023-10-16 11:38:36 +0530

    Express.js

---
+++ CHATGPT-API-Express/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+const OpenAI = require('openai');
+const messages = []
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const mySecret = process.env['mySecret'];
+const openai = new OpenAI({
+  apiKey: mySecret,
+
+ // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main(input) {
+  messages.push({ role: 'user', content: input })
+  console.log(messages)
+  const completion = await openai.chat.completions.create({
+
+    messages: messages,
+    model: 'gpt-3.5-turbo',
+    
+  });
+
+  // console.log(chatCompletion.choices);
+  return(completion.choices[0]?.message?.content);
+}
+
+// Render Html File
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'templates/index.html'));
+});
+
+
+
+app.post('/api', async function (req, res, next) {
+  console.log(req.body)
+  const mes  = await main(req.body.input)
+  res.json({success : true,message : mes})
+})
+
+
+app.listen(port, () => {
+  // Code.....
+})
